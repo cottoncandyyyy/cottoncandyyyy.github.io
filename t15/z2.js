c81744b7cedf1010e7e2d510fc1934d1e0a21a55
cottoncandyyyy <ladnukasa@gmail.com>
2021-02-27 11:20:35 +0300

    t20

---
+++ t15/z2.js
@@ -0,0 +1,39 @@
+const user = {
+    name: "Ася"
+    lastName: "Ладнюк"
+    age: 13,
+    email: "ladnukasa@gmail.com"
+    city: "Львів"
+}
+
+function showUser(){
+    const usr_data = document.getElementById('user_data');
+
+
+    usr_data.innerHTML =
+
+    <tr>
+        <td>${user.name}</td>
+        <td>${user.lastName}</td>
+        <td>${user.age}</td>
+        <td>${user.email}</td>
+        <td>${user.city}</td>
+        
+    </tr>
+}
+
+function changeName(new_name){
+    user.name = new_name;
+}
+function changeLastName(new_lastName){
+    user.lastName = new_lastName;
+}
+function changeAge(new_age){
+    user.age = new_age;
+}
+function changeEmail(new_email){
+    user.email = new_email;
+}
+function changeCity(new_city){
+    user.city = new_city;
+}
